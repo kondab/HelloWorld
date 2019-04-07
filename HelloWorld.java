e3599c53c2df11d1b3236949c33f3da665c8d6b7
Konda Balaji <balukonda@gmail.com>
2019-04-07 14:07:01 +0530

    feature 2 master direct branch

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ public class Hello {
 	
 	public static void main (String[] args){
 		System.out.println("Hello world Balaji!");
-		System.out.println("Hello world!");
+		System.out.println("Hello world! Harshith");
 		System.out.println("Hello world!");
 	}
 }
